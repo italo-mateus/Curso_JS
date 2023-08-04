a0779a74890fa73a0fbaff46a910bafaa1fe0956
Italo Mateus <italomtitus@gmail.com>
2023-08-03 23:38:47 -0300

    Create if.js

---
+++ Estrutura de controle/if.js
@@ -0,0 +1,18 @@
+function soboanoticia(nota){
+    if(nota>=7){
+        console.log('aprovado com ' + nota)
+    };
+};
+
+soboanoticia(8)
+
+function seforverdade (valor){
+    if(valor){
+        console.log('É verdade.. ' + valor )
+    }
+}
+
+seforverdade(1)
+seforverdade(null)
+seforverdade(undefined)
+
